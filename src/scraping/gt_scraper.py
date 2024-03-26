c7883a7dc861e0f8f19ac050bf15068ba0c2a2a9
pacotoh <pacgallego@gmail.com>
2024-03-26 17:18:37 +0100

    GT scraping first code version with books downloading

--- src/scraping/gt_scraper.py
+++ src/scraping/gt_scraper.py
@@ -1,38 +1,71 @@
-from bs4 import BeautifulSoup
 import requests
 import concurrent.futures
+import json
+import logging
+from datetime import datetime
 
 
-def get_book(book_id: int) -> dict | None:
-    try:
-        soup = BeautifulSoup(
-            requests
-            .get(f'https://www.gutenberg.org/cache/epub/{book_id}/pg{book_id}-images.html').text
-        )
-
-        return {'id': id, 'text': '. '.join([p.text for p in soup.find_all('p')])}
-    except:  # FIXME: What kind of exception?
-        return None
+CONFIG_JSON = 'config/gt_config.json'
+config = json.load(open(file=CONFIG_JSON, encoding='utf-8'))
+last_book_id = config['last_book_id']
+BATCH_SIZE = config['batch_size']
+LOG_FILE = datetime.now().strftime('%Y%m%d%H%M%S')
+NOT_FOUND = '<title>404 | Project Gutenberg</title>'
 
 
-def main():
-    test_books = []
+logging.basicConfig(
+    filename=f'../../logs/GT{LOG_FILE}.log',
+    level=logging.INFO,
+    format='[%(asctime)s]: %(levelname)s %(message)s'
+)
+
+
+def get_book(book_id: int):
+    content_path = str(config['pathc']).replace('{book_id}', str(book_id))
+    file_name = content_path.split('/')[-1]
+
+    response = requests.get(content_path, stream=True)
+    if NOT_FOUND not in response.text:
+        with open(f'{config["content_path"]}{file_name}', 'wb') as out_file:
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    out_file.write(chunk)
+        logging.info(msg=f'Book {book_id} added')
+    else:
+        logging.info(msg=f'Book {book_id} not available')
+
+
+def get_metadata(book_id: int):
+    metadata_path = str(config['pathm']).replace('{book_id}', str(book_id))
+    logging.info(msg=f'{metadata_path}')
+
+
+def update_book_id(current_id: int):
+    config["last_book_id"] = current_id
+    with open(CONFIG_JSON, 'w') as f:
+        json.dump(config, f, indent=2)
+
+
+def execute(function, executor, from_id, to_id):
+    future_to_url = {executor.submit(function, i): i for i in range(from_id, to_id)}
+
+    for future in concurrent.futures.as_completed(future_to_url):
+        url = future_to_url[future]
+        try:
+            future.result()
+        except Exception as e:
+            logging.info(msg='%r generated an exception: %s' % (url, e))
+
+
+def exec():
+    new_book_id = last_book_id + int(BATCH_SIZE)
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        # TODO: Parametrize the range of execution
-        future_to_url = {executor.submit(get_book, i): i for i in range(1, 3)}
-        for future in concurrent.futures.as_completed(future_to_url):
-            url = future_to_url[future]
-            try:
-                book = future.result()
-            except Exception as exc:
-                print('%r generated an exception: %s' % (url, exc))
-            else:
-                if book is not None:
-                    test_books.append(book)
+        execute(get_book, executor, last_book_id, new_book_id)
+        execute(get_metadata, executor, last_book_id, new_book_id)
 
-        print(test_books)
+    update_book_id(new_book_id)
 
 
 if __name__ == '__main__':
-    main()
+    exec()
