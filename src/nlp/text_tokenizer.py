5802dded637bd41a92b9818a70dceaa39478096d
pacotoh <pacgallego@gmail.com>
2024-04-12 10:09:00 +0200

    First text_tokenizer version with text, sent tokenization, entities, lexical values and special chars added

--- src/nlp/text_tokenizer.py
+++ src/nlp/text_tokenizer.py
@@ -1,44 +1,73 @@
 import re
-import json
+from dataclasses import dataclass, field
+from typing import ClassVar
 import nltk
 import spacy
-
-nlp = spacy.load("en_core_web_sm")
-
-
-def get_special_characters(text: str) -> str:
-    pattern = r'[a-zA-z0-9.,!?/:;\"\'\s]'
-    return re.sub(pattern, '', text)
+from spacy.pipeline.senter import Language
 
 
-def clean_text(text: str) -> str:
-    text = re.sub(r'\[.*?\]', '', text)
-    text = re.sub('–*', '', text)
-    text = re.sub('—', '', text)
-    text = re.sub(r'\n', ' ', text)
-    text = re.sub('←→', '', text)
-    text = re.sub(r'\s{2,}', ' ', text)
-    text = re.sub(' ', ' ', text)
-    return text
+@dataclass
+class TextProperties:
+    text: str = field(default='', init=True)
+    nlp: ClassVar[Language] = spacy.load("en_core_web_sm")
+    sentences: list = field(default_factory=list, init=False)
+    tokens: list = field(default_factory=list, init=False)
+    entities: dict = field(default_factory=dict, init=False)
+    lexical: dict = field(default_factory=dict, init=False)
+    special_chars: str = field(init=False)
+
+    def __post_init__(self):
+        self.doc = TextProperties.nlp(self.text)
+        self.lexical = self._lexical_attributes()
+        self._sent_tokenize()
+        self._named_entities()
+        self._special_characters()
+
+    def _special_characters(self):
+        pattern = r'[a-zA-z0-9.,!?/:;\"\'\s]'
+        self.special_chars = re.sub(pattern, '', self.text)
+
+    def _clean_text(self):
+        text = re.sub(r'\[.*?\]', '', self.text)
+        text = re.sub('–*', '', text)
+        text = re.sub('—', '', text)
+        text = re.sub(r'\n', ' ', text)
+        text = re.sub('←→', '', text)
+        text = re.sub(r'\s{2,}', ' ', text)
+        text = re.sub(' ', ' ', text)
+        self.text = text
+
+    def _sent_tokenize(self):
+        self.sentences = nltk.sent_tokenize(self.text)
+
+    def _lexical_attributes(self) -> dict:
+        return {token.text: [token.i,
+                             token.is_alpha,
+                             token.is_punct,
+                             token.like_num,
+                             token.is_stop,
+                             token.is_oov,
+                             token.pos_,
+                             token.dep_,
+                             token.head.text,
+                             token.lemma_,
+                             token.tag_,
+                             token.shape_,
+                             token.has_vector,
+                             token.vector_norm]
+                for token in self.doc}
+
+    def _named_entities(self):
+        self.entities = {ent.text: [ent.start_char, ent.end_char, ent.label_, spacy.explain(ent.label_)]
+                         for ent in self.doc.ents}
+
+    def save_to_pickle(self):
+        pass
+
+    def load_from_pickle(self):
+        pass
 
 
-def sent_tokenize(text: str) -> list[str]:
-    return nltk.sent_tokenize(clean_text(text))
-
-
-def lexical_attributes(text: str) -> dict:
-    doc = nlp(text)
-    return {token.text: [token.i,
-                         token.is_alpha,
-                         token.is_punct,
-                         token.like_num,
-                         token.pos_,
-                         token.dep_,
-                         token.head.text]
-            for token in doc}
-
-
-def named_entities(text: str) -> dict:
-    doc = nlp(text)
-    return {ent.text: (ent.label_, spacy.explain(ent.label_)) for ent in doc.ents}
-
+if __name__ == '__main__':
+    tp = TextProperties('This is an example. And this is another example speaking about Frank in New York.')
+    print(tp)
