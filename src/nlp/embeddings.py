d99f9a6b98a067528f10d691461d280c8983480b
pacotoh <pacgallego@gmail.com>
2024-04-18 21:50:52 +0200

    Method definition of minimum embeddings to implement

---
+++ src/nlp/embeddings.py
@@ -0,0 +1,24 @@
+
+
+def tf_idf() -> None:
+    pass
+
+
+def word_to_vec() -> None:
+    pass
+
+
+def fast_text() -> None:
+    pass
+
+
+def co_ocurrence_matrix() -> None:
+    pass
+
+
+def cbow() -> None:
+    pass
+
+
+def skip_gram() -> None:
+    pass
