39f1dc7bda0469d06e3e7893561ea70b8f634978
pacotoh <pacgallego@gmail.com>
2024-04-18 23:19:01 +0200

    Embeddings corpus creation. Change to dict

--- src/nlp/embeddings.py
+++ src/nlp/embeddings.py
@@ -1,3 +1,20 @@
+import os
+import json
+
+CONFIG_JSON = 'config/embeddings.json'
+config = json.load(open(file=CONFIG_JSON, encoding='utf-8'))
+INFO_BASE_PATH = config['info_base_path']
+
+
+def load_text(filename: str) -> str:
+    with open(f'{INFO_BASE_PATH}{filename}/{filename}_text.txt', 'r') as text:
+        content = text.read()
+    return content
+
+
+def create_corpus() -> list[str]:
+    files = os.listdir(f'{INFO_BASE_PATH}/')
+    return [load_text(f'{file}') for file in files]
 
 
 def tf_idf() -> None:
@@ -22,3 +39,7 @@ def cbow() -> None:
 
 def skip_gram() -> None:
     pass
+
+
+if __name__ == '__main__':
+    print(create_corpus())
