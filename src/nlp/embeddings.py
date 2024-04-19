a833beeff6cbec50c3fc4575e0edf1d483f475cf
pacotoh <pacgallego@gmail.com>
2024-04-19 11:06:55 +0200

    co-ocurrence matrix generation created without paralelism

--- src/nlp/embeddings.py
+++ src/nlp/embeddings.py
@@ -1,11 +1,16 @@
 import os
 import json
+
+import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.tokenize import word_tokenize, sent_tokenize
+import numpy as np
 
 
 CONFIG_JSON = 'config/embeddings.json'
 config = json.load(open(file=CONFIG_JSON, encoding='utf-8'))
 INFO_BASE_PATH = config['info_base_path']
+LANGUAGE = config['language']
 
 
 def load_text(filename: str) -> str:
@@ -20,9 +25,30 @@ def create_corpus(base_path: str) -> dict[str, str]:
 
 
 def tf_idf(base_path: str) -> TfidfVectorizer:
-    tfidf_vec = TfidfVectorizer(stop_words='english')
+    tfidf_vec = TfidfVectorizer(stop_words=LANGUAGE)
     corpus = create_corpus(base_path)
     return tfidf_vec.fit_transform(corpus.values())
+
+
+def co_ocurrence_matrix(corpus: list, window_size: int = 3) -> dict[str, np.array]:
+    unique_words = set()
+    for text in corpus:
+        for word in word_tokenize(text):
+            unique_words.add(word)
+
+    word_search_dict = {word: np.zeros(shape=(len(unique_words))) for word in unique_words}
+    word_list = list(word_search_dict.keys())
+    for text in corpus:
+        text_list = word_tokenize(text)
+        for idx, word in enumerate(text_list):
+            i = max(0, idx - window_size)
+            j = min(len(text_list) - 1, idx + window_size)
+            search = [text_list[idx_] for idx_ in range(i, j+1)]
+            search.remove(word)
+            for neighbor in search:
+                nei_idx = word_list.index(neighbor)
+                word_search_dict[word][nei_idx] += 1
+    return word_search_dict
 
 
 def word_to_vec() -> None:
@@ -30,10 +56,6 @@ def word_to_vec() -> None:
 
 
 def fast_text() -> None:
-    pass
-
-
-def co_ocurrence_matrix() -> None:
     pass
 
 
@@ -45,5 +67,15 @@ def skip_gram() -> None:
     pass
 
 
+def export_data(base_path: str) -> None:
+    corpus = create_corpus(base_path)
+    for text in corpus.items():
+        sentences = sent_tokenize(text[1])
+        name = text[0]
+        # TODO: Create co-ocurrence matrices concurrently
+        com = co_ocurrence_matrix(sentences)
+        pd.DataFrame(com, index=com.keys()).astype('int').to_csv(f'{base_path}{name}/{name}_com.csv')
+
+
 if __name__ == '__main__':
-    print(tf_idf(INFO_BASE_PATH))
+    export_data(INFO_BASE_PATH)
