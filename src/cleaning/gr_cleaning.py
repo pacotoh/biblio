a6bce8302b4798505d298fa676bdb6f699691269
pacotoh <pacgallego@gmail.com>
2024-04-23 00:54:52 +0200

    Basic cleaning in GR Cleaning

--- src/cleaning/gr_cleaning.py
+++ src/cleaning/gr_cleaning.py
@@ -2,6 +2,8 @@ import datetime
 from datetime import datetime
 import pandas as pd
 import glob
+import ast
+import numpy as np
 
 GR_DATA_PATH = '../../data/gr'
 
@@ -17,6 +19,44 @@ def export_data(df: pd.DataFrame) -> None:
     df.to_csv(f'{GR_DATA_PATH}/{date}.csv')
 
 
+def clean_first_step(df: pd.DataFrame) -> pd.DataFrame:
+    info = df.drop(['title',
+                    'desc',
+                    'pub_info',
+                    'cover',
+                    'id',
+                    'publication_timestamp',
+                    'isbn',
+                    'isbn13'], axis=1)
+
+    info['authors'] = info['authors'].map(lambda x: set(ast.literal_eval(x)))
+    info['rating_value'] = info['rating_value'].map(lambda x: int(x*100))
+    info['num_pages'] = info['num_pages'].convert_dtypes()
+    info['num_pages'] = info['num_pages'].replace('<NA>', np.nan)
+    info = info[~info['publisher'].isna()]
+
+    return info
+
+
+def clean_counts(df: pd.DataFrame) -> pd.DataFrame:
+    df['rating_count'] = df['rating_count'].convert_dtypes()
+    df.fillna({'rating_count': 0}, inplace=True)
+
+    df['review_count'] = df['review_count'].convert_dtypes()
+    df.fillna({'review_count': 0}, inplace=True)
+
+    return df
+
+
+def clean_genres(df: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+
+def clean_format(df: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+
 if __name__ == '__main__':
     data = join_data()
-    export_data(data)
+    df_cleaned = clean_first_step(data)
+    print(df_cleaned)
