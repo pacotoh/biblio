4ae83a6b3e7686172bbb68ecaf3604e403315432
pacotoh <pacgallego@gmail.com>
2024-04-25 21:23:30 +0200

    Two versions of video transcription with whisper. Need to configure CUDA in order to fix only GPU exec

--- src/audio/summarizer/summarizer.py
+++ src/audio/summarizer/summarizer.py
@@ -1,10 +1,36 @@
+import torch
 from moviepy.editor import VideoFileClip
 import os
-
+import whisper
+import subprocess
 
 VIDEO_PATH = 'video/'
 AUDIO_PATH = 'audio/'
 TEXT_PATH = 'text/'
+
+
+def transcribe_video_command() -> None:
+    videos = os.listdir(VIDEO_PATH)
+    for video_name in videos:
+        print(f'Running whisper transcription for {video_name}...')
+        command = ["whisper",
+                   "--model", "large",
+                   "--language", "es",
+                   "--task", "transcribe",
+                   f"{VIDEO_PATH}{video_name}"]
+
+        result = subprocess.run(command, capture_output=True, text=True)
+        with open(f'{TEXT_PATH}{video_name}') as transcription:
+            transcription.write(result.stdout)
+
+
+def transcribe_video() -> None:
+    videos = os.listdir(VIDEO_PATH)
+    for video_name in videos:
+        print(f'Running whisper transcription for {video_name}...')
+        result = model.transcribe(f'{VIDEO_PATH}{video_name}')
+        with open(f'{TEXT_PATH}{video_name}') as transcription:
+            transcription.write(result['text'])
 
 
 def extract_audio() -> None:
@@ -15,10 +41,10 @@ def extract_audio() -> None:
         audio.write_audiofile(f'{AUDIO_PATH}{video_name.split(".")[0]}.ogg')
 
 
-def transcribe_audio() -> None:
-    # TODO: Use whisper to transcribe audio and save into text
-    pass
-
-
 if __name__ == '__main__':
-    extract_audio()
+    if torch.cuda.is_available():
+        model = whisper.load_model('large', device='cuda')
+    else:
+        model = whisper.load_model('small')
+
+    transcribe_video()
